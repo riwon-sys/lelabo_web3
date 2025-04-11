36219e390961a422ac1fe56d48bd439df2f417ee
kim ri won <lelabo7317@gmail.com>
2025-04-11 10:04:15 +0900

    deployJPA 과제6: '과제4(04/07) 비품 관리 CRUD  과제를 이용하여 flutter 앱 화면 구현하기.| rw 25-04-10

--- src/main/java/example/class02/_toDto/ExamEntity1Repository.java
+++
@@ -1,9 +0,0 @@
-package example.class02._toDto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExamEntity1Repository extends JpaRepository< ExamEntity1 , Integer> {
-
-}
--- src/main/java/example/class02/_영속성/ExamRepository.java
+++
@@ -1,9 +0,0 @@
-package example.class02._영속성;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExamRepository extends JpaRepository<ExamEntity,Integer> {
-}
